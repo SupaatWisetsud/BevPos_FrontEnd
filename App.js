f2bde97b7ef028eacd6800d68cc542a81143abf3
supaatwisetsud <600112416008@bru.ac.th>
2020-05-25 21:04:23 +0700

    connect apollo server

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Text } from "native-base";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
+  
   return (
     <View>
       <Icon name="home" size={18} />
--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+import React from 'react';
 import { registerRootComponent } from 'expo';
-
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient, InMemoryCache } from 'apollo-boost';
+import { setContext } from 'apollo-link-context'
+import { createUploadLink } from 'apollo-upload-client';
 import App from './App';
+
+const host = "http://10.0.2.2:4000/graphql";
+
+const authLink = setContext(( _ , { headers } ) => {
+    return {
+        headers: {
+            ...headers
+        }
+    }
+});
+
+const httpLink = createUploadLink({uri: host});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+})
+const MapToAplloServer = () => <ApolloProvider client={client} ><App /></ApolloProvider> 
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(MapToAplloServer);
