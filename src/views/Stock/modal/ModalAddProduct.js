610145a39a68a0f7f23dbe759ac74b95536a7a44
supaatwisetsud <600112416008@bru.ac.th>
2020-05-27 17:48:58 +0700

    modal add product success

--- src/views/Stock/modal/ModalAddProduct.js
+++ src/views/Stock/modal/ModalAddProduct.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Modal, View, Text, StyleSheet } from 'react-native';
-import { Button, Input, Content, H2, Item, Label } from 'native-base';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Button, Input, Content, H2, Item, Label, Picker } from 'native-base';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function ModalAddProduct({
     show = false,
@@ -19,23 +20,70 @@ export default function ModalAddProduct({
         >
             <View style={styles.centeredView}>
                 <View style={styles.modalView}>
-                    <Button 
-                        danger 
-                        small 
-                        style={{position: "absolute", right: 0, paddingHorizontal: 10}}
-                        onPress={onClose}
-                    >
-                        <Icon name="times" color="white" size={18} />
-                    </Button>
-                    <H2>เพิ่มสินค้า</H2>
-                    <Item last style={{marginTop: 30}}>
-                        <Icon name="home" size={28} />
-                        <Input placeholder="ชื่อสินค้า" />
-                    </Item>
-                    <Item style={{marginTop: 20}}>
-                        <Icon name="home" size={28} />
-                        <Input keyboardType="numeric" placeholder="ราคา" />
-                    </Item>
+                    <View style={{position: "absolute", zIndex: 10, right: 0}}>
+                        <Button small danger block style={{paddingHorizontal: 10}} onPress={onClose}>
+                            <Icon color="white" name="times" />
+                        </Button>
+                    </View>
+                    <ScrollView>
+                        <View style={{padding: 35}}>
+                            <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+                                <H2>เพิ่มสินค้า</H2>
+                            </View>
+                            <Item last style={{marginTop: 30}}>
+                                <Icon name="file-signature" size={28} />
+                                <Input placeholder="ชื่อสินค้า" />
+                            </Item>
+                            <Item style={{marginTop: 20}}>
+                                <Icon name="tags" size={28} />
+                                <Input keyboardType="numeric" placeholder="ราคาต้นทุน" />
+                            </Item>
+                            <Item style={{marginTop: 20}}>
+                                <Icon name="money-bill-alt" size={28} />
+                                <Input keyboardType="numeric" placeholder="ราคาขาย" />
+                            </Item>
+                            <Item style={{marginTop: 20}}>
+                                <Icon name="cubes" size={28} />
+                                <Input keyboardType="numeric" placeholder="จำนวนสินค้า" />
+                            </Item>
+                            <Item style={{marginTop: 20}}>
+                                <Label>แจ้งเตือนเมื่อสินค้าน้อยกว่า: </Label>
+                                <Picker
+                                    note
+                                    mode="dropdown"
+                                    // style={{ width: 120 }}
+                                    // selectedValue={this.state.selected}
+                                    // onValueChange={this.onValueChange.bind(this)}
+                                >
+                                    <Picker.Item label="5" value={5} />
+                                    <Picker.Item label="10" value={10} />
+                                    <Picker.Item label="15" value={15} />
+                                    <Picker.Item label="20" value={20} />
+                                </Picker>
+                            </Item>
+                            <View style={{marginTop: 20}}>
+                                <Text style={{marginBottom: 5}}>รูปสินค้า</Text>
+                                <Button info block>
+                                    <Text style={{fontSize: 18, color: "white"}}>Upload image</Text>
+                                </Button>
+                            </View>
+                            <Item style={{marginTop: 20}}>
+                                <Icon name="barcode" size={28} />
+                                <Input placeholder="barcode" />
+                                <Button warning block style={{paddingHorizontal: 5}}>
+                                    <Icon name="camera" size={28} />
+                                </Button>
+                            </Item>
+                            <View style={{marginTop: 20, flexDirection: "row", justifyContent: "center"}}>
+                                <Button success block style={{paddingHorizontal: 10, marginRight: 10}}> 
+                                    <Text style={{fontSize: 18, color: "white"}}>บันทึก</Text>
+                                </Button>
+                                <Button danger block style={{paddingHorizontal: 10}} onPress={onClose}>
+                                    <Text style={{fontSize: 18, color: "white"}}>ยกเลิก</Text>
+                                </Button>
+                            </View>
+                        </View>
+                    </ScrollView>
                 </View>
             </View>
         </Modal>
@@ -53,7 +101,6 @@ const styles = StyleSheet.create({
         margin: 20,
         backgroundColor: "white",
         borderRadius: 5,
-        padding: 35,
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
